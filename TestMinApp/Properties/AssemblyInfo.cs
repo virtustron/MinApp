6174a4fe885b16ad76d58b710db842e36718c7a1
Vitalii Tron <virtustron@gmail.com>
2021-12-10 13:20:13 +0200

    Add project files.

---
+++ TestMinApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestMinApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestMinApp")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("878cd69f-9780-4c8e-9d58-e54db71790df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestMinApp/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using MinApp;
+
+namespace TestMinApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMinSuccesAverage()
+        {
+            int min;
+
+            min = Program.Min(3, 2, 5);
+
+            Assert.AreEqual(2, min);
+        }
+
+        [TestMethod]
+        public void TestMinSuccesBigPositives()
+        {
+            int min;
+
+            min = Program.Min(32457, 2784, 5687);
+
+            Assert.AreEqual(2784, min);
+        }
+
+        [TestMethod]
+        public void TestMinSuccesBigNegatives()
+        {
+            int min;
+
+            min = Program.Min(-32457, -2784, -5687);
+
+            Assert.AreEqual(-32457, min);
+        }
+
+        [TestMethod]
+        public void TestMinSuccesPositivesAndNegatives()
+        {
+            int min;
+
+            min = Program.Min(+32457, -2784, +5687);
+
+            Assert.AreEqual(-2784, min);
+        }
+    }
+}
